88640246aa6bb0321fe2b0c254834b925a6319ec
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2023-01-06 10:01:17 -0300

    feat: include prettier and eslint rules

--- src/main.js
+++ src/main.js
@@ -3,11 +3,7 @@ import { render } from 'terezzu'
 import { appMain } from './components/appMain'
 import { appHello } from './components/appHello'
 
-render(
-  appMain,
-  'main',
-  (appMainElement) => {
-    render(appHello, 'hello', null, appMainElement)
-  },
-  document.body
-)
+// prettier-ignore
+render(appMain, 'main', appMainElement => {
+  render(appHello, 'hello', null, appMainElement)
+}, document.body)
